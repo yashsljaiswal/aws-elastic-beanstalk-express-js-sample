a13f1a66615224b2e94daa9087daa85d95d855f8
Yash Jaiswal <yashsunil.jaiswal_hcl@kantar.com>
2023-01-31 19:39:48 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!! to AWS by YAJAI'));
+app.get('/', (req, res) => res.send('Hello World!! to AWS by YAJAI Pushing Code 2 time'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
